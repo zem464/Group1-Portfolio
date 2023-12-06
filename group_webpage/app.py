418f84bfd86a3f38a7d0d3b36d80b2b30432c526
Iris Membrere <irismembrere@gmail.com>
2023-12-06 15:27:52 +0800

    Add

---
+++ group_webpage/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/works', methods=['GET', 'POST'])
+def works():
+    result = None
+    if request.method == 'POST':
+        input_string = request.form.get('inputString', '')
+        result = input_string.upper()
+    return render_template('touppercase.html', result=result)
+
+@app.route('/contact')
+def contact():
+    return "Contact Page. please create me an html page with dummy contact info"
+
+if __name__ == "__main__":
+    app.run(debug=True)
